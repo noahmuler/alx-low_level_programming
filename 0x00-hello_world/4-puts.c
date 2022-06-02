9f91d2e2e14e21e12a8eca2d120c194d115d05c2
noahmuler <noahtaddese@gmail.com>
2022-06-02 10:18:16 -0700

    puts function in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * Progrma to print th following:
+ * Programming is like building a multilingual puzzle
+ *
+ */
+
+int main(void)
+{
+	/*
+	 * This function calls puts function
+	 * to desplay a string value held in pointer text
+	 *
+	 */
+
+	char *text = "Programming is like building a multilingual puzzle\0";
+
+	puts(text);
+	return (0);
+}
