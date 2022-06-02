e6fc8c9b6426c65eaeb2a0176f3c5f00f0ddd876
noahmuler <noahtaddese@gmail.com>
2022-06-02 12:39:38 -0700

    quote file added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\0", 60, 1, stderr);
+	return (1);
+}
