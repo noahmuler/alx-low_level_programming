380046eec97055396956045757d6c007db48513c
noahmuler <noahtaddese@gmail.com>
2022-06-02 12:03:23 -0700

    & added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	lli = sizeof(long long int);
 	f = sizeof(float);
 
-	printf("Size of a char: ", c, " byte(s)\n");
-	printf("Size of an int: ", i, " byte(s)\n");
-	printf("Size of a king int", li, " byte(s)\n");
-	printf("Size of a long long int: ", lli, " byte(s)\n");
-	printf("Size of a float: ", f, " byte(s)\n");
+	printf("Size of a char: ", &c, " byte(s)\n");
+	printf("Size of an int: ", &i, " byte(s)\n");
+	printf("Size of a king int", &li, " byte(s)\n");
+	printf("Size of a long long int: ", &lli, " byte(s)\n");
+	printf("Size of a float: ", &f, " byte(s)\n");
 	return (0);
 }
