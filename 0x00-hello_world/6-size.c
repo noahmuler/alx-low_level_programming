f14943ef03512d5d65004ec31bac2d557ca11fb9
noahmuler <noahtaddese@gmail.com>
2022-06-02 12:07:10 -0700

    variable changed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int c, i, li, lli, f;
+	int ch, i, li, lli, f;
 
-	c = sizeof(char);
+	ch = sizeof(char);
 	i = sizeof(int);
 	li = sizeof(long int);
 	lli = sizeof(long long int);
 	f = sizeof(float);
 
-	printf("Size of a char: ", &c, " byte(s)\n");
+	printf("Size of a char: ", &ch, " byte(s)\n");
 	printf("Size of an int: ", &i, " byte(s)\n");
-	printf("Size of a king int", &li, " byte(s)\n");
+	printf("Size of a long int: ", &li, " byte(s)\n");
 	printf("Size of a long long int: ", &lli, " byte(s)\n");
 	printf("Size of a float: ", &f, " byte(s)\n");
 	return (0);
