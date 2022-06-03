6875353a2f64c7db1e1c581d7212ebad62d72230
noahmuler <noahtaddese@gmail.com>
2022-06-03 02:34:56 -0700

    more restrictions

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = '0'; dig <= '9'; dig++)
+		putchar(dig);
+	putchar('\n');
+	return (0);
+}
