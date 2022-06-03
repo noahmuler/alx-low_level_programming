8c839430fc1860ccd4e86b46061bb43cd0243d9d
noahmuler <noahtaddese@gmail.com>
2022-06-03 03:39:54 -0700

    b16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = '0'; dig <= '9'; dig++)
+		putchar(dig);
+	for (dig = 'a'; dig <= 'f'; dig++)
+		putchar(dig);
+	putchar('\n');
+	return (0);
+}
