811e0f939ef472d0d7f2a415de2042fa7b64adde
noahmuler <noahtaddese@gmail.com>
2022-06-06 03:01:58 -0700

    used less for loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,35 +7,23 @@
  */
 int main(void)
 {
-	int dig1, dig2, dig3, dig4;
+	int dig1, dig2;
 
-	dig1 = '0';
-	dig2 = '0';
-	dig3 = '0';
-	dig4 = '0';
-
-	for (dig1 = '0'; dig1 <= '9'; dig1++)
+	for (dig1 = 0; dig1 <= 99; dig1++)
 	{
-		for (dig2 = '0'; dig2 <= '9'; dig2++)
+		for (dig2 = 0; dig2 <= 99; dig2++)
 		{
-			for (dig3 = '0'; dig3 <= '9'; dig3++)
+			if (dig1 < dig2 && dig1 != dig2)
 			{
-				for (dig4 = '0'; dig4 <= '9'; dig4++)
+				putchar((dig1 / 10) + '0');
+				putchar((dig1 % 10) + '0');
+				putchar(' ');
+				putchar((dig2 /10) + '0');
+				putchar((dig2 % 10) + '0');
+				if (dig1 != 98 || dig2 == 99)
 				{
-					if ((dig2 < dig4) && (dig1 <= dig3))
-					{
-						putchar(dig1);
-						putchar(dig2);
-						putchar(' ');
-						putchar(dig3);
-						putchar(dig4);
-					}
-					if (!(dig1 == '9' && dig2 == '8' && dig3 == '9' && dig4 == '9')
-						&& (dig2 < dig4) && (dig1 <= dig3))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				putchar(',');
+				putchar(' ');
 				}
 			}
 		}
