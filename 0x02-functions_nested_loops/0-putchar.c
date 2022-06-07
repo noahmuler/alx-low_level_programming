1822ac2a61d83e1105854642968c913eca740512
noahmuler <noahtaddese@gmail.com>
2022-06-07 04:23:47 -0700

    header and c file added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char pr[] = "_putchar\n";
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (_putchar(pr[i]) == 1)
+			continue;
+	}
+	return (0);
+}
