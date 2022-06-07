83a2c95fbd27faecb68d80ca94a2906d7ae132a7
noahmuler <noahtaddese@gmail.com>
2022-06-07 09:51:52 -0700

    add added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - Entry point
+ *
+ * @m: passed as int
+ * @n: passed as int
+ * Return: Always 0 (Success)
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
