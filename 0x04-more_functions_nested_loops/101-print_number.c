e254dd9042d444ac31a2c8ad7c597cd536cb5647
noahmuler <noahtaddese@gmail.com>
2022-06-09 06:15:05 -0700

    while used

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,41 +8,27 @@
  */
 void print_number(int n)
 {
-	if (n >= 10 && n < 100)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 1000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-	}
-	else if (n < 0 && n >= -9)
+	int d = 1, i = 0, ii = 0;
+
+	if (n < 0)
 	{
 		_putchar('-');
-		n = -1 * n;
-		_putchar(n + '0');
+		n = -n;
 	}
-	else if (n < -9 && n >= -99)
+	while (n / d != 0)
 	{
-		_putchar('-');
-		n = -1 * n;
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		d *= 10;
+		i++;
 	}
-	else
-		_putchar(n + '0');
+	d = d / 10;
+
+	while (ii < i)
+	{
+		_putchar('0' + n / d);
+		n = n - (n / d) * d;
+		d = d / 10;
+		ii++;
+	}
+	if (i == 0)
+		_putchar('0' + n);
 }
