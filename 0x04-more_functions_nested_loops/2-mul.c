01cfc4a8cab1a00a34ba5c0463a3a50f64032425
noahmuler <noahtaddese@gmail.com>
2022-06-09 02:05:42 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function to check uppercase character
+ *
+ * @a: int var passed
+ * @b: int arg passed
+ * Return: 0 or 1
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
