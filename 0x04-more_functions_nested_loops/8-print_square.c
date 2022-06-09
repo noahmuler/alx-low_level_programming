d4b9cb952892a1df0edb9bf9eaa41d17aea5e697
noahmuler <noahtaddese@gmail.com>
2022-06-09 03:13:02 -0700

    square added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - function called
+ *
+ * @size: int num arg
+ * Return: 0 or 1
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
