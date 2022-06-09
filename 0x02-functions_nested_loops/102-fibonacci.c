8fb3017e5cdd1d6796779c80d0e7ba06320d8556
noahmuler <noahtaddese@gmail.com>
2022-06-08 23:22:23 -0700

    func avoided

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	unsigned long int fib = 0, fib2 = 1, sum;
+
+	for (n = 1; n <= 50; n++)
+	{
+		sum = fib + fib2;
+		printf("%lu", sum);
+
+		fib = fib2;
+		fib2 = sum;
+
+		if (n == 50)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
