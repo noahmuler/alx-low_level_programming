29c9f5f7804a4ad8525466461c67dfe7f36a6c77
noahmuler <noahtaddese@gmail.com>
2022-06-13 08:11:22 -0700

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - puts half of string passed on stdout
+ *
+ * @str: arg passed as pointer
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	unsigned long i;
+
+	if ((strlen(str) % 2) == 0)
+		for (i = (strlen(str) / 2); i < strlen(str); i++)
+			printf("%c", str[i]);
+	else
+		for (i = ((strlen(str) - 1) / 2); i < strlen(str); i++)
+			printf("%c", str[i]);
+	printf("\n");
+}
