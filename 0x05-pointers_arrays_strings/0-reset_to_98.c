463b5921ee189690585b5d786addfcf27bf074cf
noahmuler <noahtaddese@gmail.com>
2022-06-13 04:50:48 -0700

    set v to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets value to 98
+ *
+ * @n: arg passed as pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
