b3af1496ce113118e1b818019a43bdf86af767c9
noahmuler <noahtaddese@gmail.com>
2022-06-13 08:33:01 -0700

    string copied

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - returns pointer
+ *
+ * @dest: arg passed as dest pointer
+ * @src: arg passed as pointer
+ * Return: int of 0 or 1
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
