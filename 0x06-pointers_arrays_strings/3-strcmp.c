8a7180a1f099cb54d94129ca22e688311bab1a3f
noahmuler <noahtaddese@gmail.com>
2022-06-15 02:17:11 -0700

    string compared

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - concatination function
+ *
+ * @s1: string pointer
+ * @s2: string pointer
+ * Return: int 0 or 1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
