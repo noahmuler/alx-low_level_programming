cb9b04e044bb5babca0103e9bd4688889b590ecd
noahmuler <noahtaddese@gmail.com>
2022-06-15 01:58:33 -0700

    concatted

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatination function
+ *
+ * @dest: destination pointer
+ * @src: source pointer
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
