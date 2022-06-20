54185e2640299261bffbde1ebea53dd59c70995f
noahmuler <noahtaddese@gmail.com>
2022-06-20 03:09:16 -0700

    strchr used

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - fills memory with constant byte
+ *
+ * @s: character pointer
+ * @c: character argument
+ * Return: char pointer
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
