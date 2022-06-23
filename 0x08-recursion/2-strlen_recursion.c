9a2816b0ccf33aa5685d8ef01be2391c361298d7
noahmuler <noahtaddese@gmail.com>
2022-06-23 04:51:47 -0700

    strlen used

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints length of a string
+ *
+ * @s: char pointer
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
