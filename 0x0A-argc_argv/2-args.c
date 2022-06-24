bf08837dea36ae008fe63e76a82f72b3bc36cf38
noahmuler <noahtaddese@gmail.com>
2022-06-24 04:38:59 -0700

    all argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument string array
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
