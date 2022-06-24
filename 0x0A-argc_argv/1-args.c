03b330a9eaa57bbbab8ffc8ba687459a6420abfb
noahmuler <noahtaddese@gmail.com>
2022-06-24 04:33:32 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument string array
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
