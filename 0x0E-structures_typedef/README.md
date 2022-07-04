1ddbe262d9657b07e7be5081aec23c3268c1d26f
noahmuler <noahtaddese@gmail.com>
2022-07-04 10:34:35 -0700

    struct typedef initialized

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is a README file for the 0x0E-Structure and typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef dog_h
+#define dog_h
+
+#include "dog.h"
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
