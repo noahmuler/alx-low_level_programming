e46aae58bcab1596a62e0fc66853c90ef086e811
noahmuler <noahtaddese@gmail.com>
2022-07-04 11:17:07 -0700

    Null handled

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,14 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Name: %f\n", d->age);
-		printf("Name: %s\n", d->owner);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
