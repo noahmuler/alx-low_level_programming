efc920288e18f211908595727051e1402f4658e0
noahmuler <noahtaddese@gmail.com>
2022-07-04 12:16:18 -0700

    freed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,7 @@ typedef struct dog
 	char *owner;
 } dog;
 typedef struct dog dog_t;
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
