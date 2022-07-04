85b000b2c67c4c06729275715d7344b57e4b679a
noahmuler <noahtaddese@gmail.com>
2022-07-04 09:31:18 -0700

    sum macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef _4_sum_h
+#define _4_sum_h
+
+#include "4-sum.h"
+/**
+ * SUM - absoulte value
+ * @X: double arg
+ * @Y: double arg
+ * Return: double
+ */
+#define SUM(X, Y)	((X) + (Y))
+
+#endif
