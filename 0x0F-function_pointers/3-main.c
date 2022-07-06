04a2ee29e72e4335c9c213b4405e151c2a525c81
noahmuler <noahtaddese@gmail.com>
2022-07-06 04:24:35 -0700

    new ptr

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,14 +7,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]), b = atoi(argv[3]), result;
+	int a = atoi(argv[1]), b = atoi(argv[3]), result, (*func)(int, int);
+	char o = *argv[2];
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	result = get_op_func(argv[2])(a, b);
+	func = get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = func(a, b);
 	printf("%d\n", result);
 	return (0);
 }
