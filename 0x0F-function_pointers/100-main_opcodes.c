54cc91900dcd0f98331e90fe8de61489f683c6d8
noahmuler <noahtaddese@gmail.com>
2022-07-06 04:15:37 -0700

    advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include "function_pointers.h"
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg variable string
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
