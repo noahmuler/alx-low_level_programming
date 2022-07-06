9cdfdf913d61e0cccb3ccd48d9caa2eddfb2f11f
noahmuler <noahtaddese@gmail.com>
2022-07-06 02:09:26 -0700

    task 3 files added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+/**
+ * main - Entry Point
+ * @argc: arg count
+ * @argv: arg elements array
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]), b = atoi(argv[3]), result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][1] == '/' || argv[2][1] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = (*get_op_func(*(argv + 2)))(a, b);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - returns the sum of a and b
+ * @a: num 1
+ * @b: num 2
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return ((a) + (b));
+}
+/**
+ * op_sub - returns the subtraction of a and b
+ * @a: num 1
+ * @b: num 2
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+/**
+ * op_mul - returns the multiplcation of a and b
+ * @a: num 1
+ * @b: num 2
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+/**
+ * op_div - returns the division of a over b
+ * @a: num 1
+ * @b: num 2
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return ((a) / (b));
+}
+/**
+ * op_mod - returns the remainder of a over b
+ * @a: num 1
+ * @b: num 2
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return ((a) % (b));
+}
