616b90c0a6bdc2a2a0d7f34ea1a291219fe51dae
noahmuler <noahtaddese@gmail.com>
2022-07-07 02:10:04 -0700

    condition fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,7 +8,7 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	va_list ap;
 	char *str;
 
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char *);
-		if (str == NULL)
+		if (!str)
 			printf("(nil)");
 		printf("%s", str);
 		if (i != (n - 1) && separator != NULL)
